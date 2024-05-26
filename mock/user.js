f76ac41c1f8f6927f127b1b965aed14b735f714e
s1ient <2518932658@qq.com>
2024-05-26 20:26:49 +0800

    11

---
+++ mock/user.js
@@ -0,0 +1,93 @@
+
+const tokens = {
+  admin: {
+    token: 'admin-token'
+  },
+  teacher: {
+    token: 'teacher-token'
+  },
+  student: {
+    token: 'student-token'
+  }
+}
+
+const users = {
+  'admin-token': {
+    roles: ['admin'],
+    introduction: 'I am a super administrator',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Super Admin'
+  },
+  'teacher-token': {
+    roles: ['teacher'],
+    introduction: 'I am a teacher',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Normal teacher'
+  },
+  'student-token': {
+    roles: ['student'],
+    introduction: 'I am a student',
+    avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
+    name: 'Normal student'
+  }
+}
+
+module.exports = [
+  // user login
+  {
+    url: '/deepLearning/user/login',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (!token) {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  },
+
+  // get user info
+  {
+    url: '/deepLearning/user/info\.*',
+    type: 'get',
+    response: config => {
+      const { token } = config.query
+      const info = users[token]
+
+      // mock error
+      if (!info) {
+        return {
+          code: 50008,
+          message: 'Login failed, unable to get user details.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: info
+      }
+    }
+  },
+
+  // user logout
+  {
+    url: '/deepLearning/user/logout',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  }
+]
